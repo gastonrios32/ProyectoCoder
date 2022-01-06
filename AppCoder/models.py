9912d92c14712a35550332cac8d0539b00eeca52
Gaston Rios <gastonrios32@gmail.com>
2022-01-06 19:58:42 -0300

    First version

---
+++ AppCoder/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre=models.CharField( max_length=50)
+    camada=models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre=models.CharField( max_length=50)
+    apellido=models.CharField( max_length=50)
+    email=models.EmailField( max_length=254)
+
+class Profesor(models.Model):
+    nombre=models.CharField( max_length=50)
+    apellido=models.CharField( max_length=50)
+    email=models.EmailField( max_length=254)
+    profesion=models.CharField( max_length=50)
+
+class Entregable (models.Model):
+    nombre=models.CharField( max_length=50)
+    fechaEntrega = models.DateField()
+    entregado = models.BooleanField()
+
