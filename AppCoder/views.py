5655b350ce4381e13d21fa8d95c46f7509459798
Gaston Rios <gastonrios32@gmail.com>
2022-01-07 22:52:31 -0300

    Se agrega nuevos templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,18 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio (request):
+    return render(request,'AppCoder/inicio.html')
+
+def cursos (request):
+    return HttpResponse('vista cursos')
+
+def profesores (request):
+    return HttpResponse('vista profesores')
+
+def estudiantes (request):
+    return HttpResponse('vista estudiantes')
+
+def entregables (request):
+    return HttpResponse('vista entregables')
